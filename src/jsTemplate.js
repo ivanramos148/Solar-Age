bcfdd06699b78fc1fedc9fc3a3172a6c924db766
MadameMadman <nico.daunt@gmail.com>
2018-03-12 20:06:45 -0700

    unbroke it

--- src/jsTemplate.js
+++
@@ -1,4 +0,0 @@
-export function jsTemplate() {
-  var x = document.getElementById("name").name;
-     alert("The name of the text field is: " + x);
-}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,1 @@
 import './styles.css';
-import { jsTemplate } from './jsTemplate';
-
-$(document).ready(function() {
-  $('#template-form').submit(function(event) {
-    event.preventDefault();
-    var output = jsTemplate(name);
-    $('#outputName').append("<li>hello my name is " + output + "</li>");
- });
-});
